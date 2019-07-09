95c0e89b5e286e8f184a4739557403847d7dc983
sunbrown <sunbrown007@gmail.com>
2019-07-09 17:08:38 +0800

    用#分隔行

--- PPT批量插入文本.py
+++ PPT批量插入文本.py
@@ -4,7 +4,7 @@ import csv
 from pptx.enum.text import PP_ALIGN
 from pptx.dml.color import RGBColor
 
-prs = Presentation(r'd:\Users\sunbr\Desktop\test.pptx')
+prs = Presentation(r'd:\Users\sunbrown\Desktop\胸牌模板 - 副本.pptx')
 pages = 0
 
 w = Inches(3.3074)
@@ -16,30 +16,28 @@ height = 0.33*h
 
 
 # names = []
-filename = r'd:\Users\sunbr\Desktop\第一届中国计算与认知神经科学会议会议7.csv'
+filename = r'd:\Users\sunbrown\Desktop\名单 - 副本.csv'
 f = open(filename, encoding='UTF-8')
 reader = csv.reader(f)
 for slide, row in zip(prs.slides, reader):
-    # pages += 1
-    # if pages < 114:
-    #     continue
     txBox = slide.shapes.add_textbox(left, top, width, height)
     tf = txBox.text_frame
     tf.paragraphs[0].text = row[0]  # 插入第一段文本
     tf.paragraphs[0].font.bold = True
     tf.paragraphs[0].font.size = Pt(36)
     tf.paragraphs[0].font.color.rgb = RGBColor(32, 56, 100)
-    # tf.paragraphs[0].font = '微软雅黑'
     tf.paragraphs[0].alignment = PP_ALIGN.CENTER
-    p = tf.add_paragraph()  # 插入第二段文本
-    p.text = row[1]
-    p.font.bold = True
-    p.font.size = Pt(28)
-    p.font.color.rgb = RGBColor(32, 56, 100)
-    p.alignment = PP_ALIGN.CENTER
+    str1 = row[1].split('#')
+    for s in str1:
+        p = tf.add_paragraph()  # 插入第二段文本
+        p.text = s
+        p.font.bold = True
+        p.font.size = Pt(20)
+        p.font.color.rgb = RGBColor(32, 56, 100)
+        p.alignment = PP_ALIGN.CENTER
 
     # tf.fit_text(max_size=Pt(40))  # 自动设置文本字体大小，原模块不支持中文，需要更改
 
 
-prs.save(r"d:\Users\sunbr\Desktop\result.pptx")
+prs.save(r"d:\Users\sunbrown\Desktop\result.pptx")
 f.close()
